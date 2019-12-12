42137f77a4264c3a68c295daa83ebe8c14bcb4fa
John Balbin <jbalbin3@yahoo.com>
2019-12-12 15:03:55 -0800

    filled in tree function

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,38 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = {
+    value: value,
+    children: []
+  };
+  _.extend(child, treeMethods);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var isFound = false;
+  var checkTree = function(node) {
+    if(!isFound) {
+      if(node.value === target) {
+        isFound = true;
+        return;
+      }
+      for(var i = 0; i < node.children.length; i++) {
+        checkTree(node.children[i]);
+      }
+    }
+  };
+  checkTree(this);
+  return isFound;
 };
 
 
